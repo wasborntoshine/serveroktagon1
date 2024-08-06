04212ce283697508f50f3d25367f265d1d5ae12c
wasborntoshine <shinebeats735@gmail.com>
2024-08-06 16:54:19 +0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,18 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '7304708558:AAFJmiQ6jwyyFrAPR-gNnJVUMp4dc1hHv0Y';
+
+const bot = new TelegramBot(token,{polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+	const chatId = msg.chat.id;
+	const resp = match[1];
+
+	bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+	const chatId = msg.chat.id;
+
+	bot.sendMessage(chatId, 'Привет, октагон!');
+});
