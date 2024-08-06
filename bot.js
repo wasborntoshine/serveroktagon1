cc73770fbf26481a2a0a124e1803eacd2052ebd7
wasborntoshine <shinebeats735@gmail.com>
2024-08-06 17:36:15 +0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,18 +1,87 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '';
+const token = '7304708558:AAFJmiQ6jwyyFrAPR-gNnJVUMp4dc1hHv0Y';
 
 const bot = new TelegramBot(token,{polling: true});
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-	const chatId = msg.chat.id;
-	const resp = match[1];
+const commands = [
 
-	bot.sendMessage(chatId, resp);
-});
+    {
 
-bot.on('message', (msg) => {
-	const chatId = msg.chat.id;
+        command: "start",
+        description: "Запуск бота"
 
-	bot.sendMessage(chatId, 'Привет, октагон!');
-});
+    },
+
+    {
+
+        command: "help",
+        description: "Раздел помощи"
+
+    },
+    {
+
+        command: "site",
+        description: "ссылка на сайт октагон"
+
+    },
+    {
+
+        command: "creator",
+        description: "фио разраба"
+
+    },
+
+]
+
+bot.setMyCommands(commands);
+
+bot.on('text', async msg => {
+
+    try {
+
+        if(msg.text.startsWith('/start')) {
+            
+            await bot.sendMessage(msg.chat.id, `Вы запустили бота!`);
+
+            if(msg.text.length > 6) {
+
+                const refID = msg.text.slice(7);
+
+                await bot.sendMessage(msg.chat.id, `Вы зашли по ссылке пользователя с ID ${refID}`);
+
+            }
+
+        }
+        else if(msg.text == '/help') {
+
+            await bot.sendMessage(msg.chat.id, `Раздел помощи`);
+
+        }
+        else if(msg.text == '/site') {
+
+            await bot.sendMessage(msg.chat.id,'[Сайт Октагон](https://students.forus.ru/?ysclid=lzi7fy42w980728758)',{
+		 parse_mode: "MarkdownV2"
+	    });
+
+        }
+        else if(msg.text == '/creator') {
+
+            await bot.sendMessage(msg.chat.id, `Ткачук Юрий`);
+
+        }
+        else {
+
+            await bot.sendMessage(msg.chat.id, msg.text);
+
+        }
+
+    }
+    catch(error) {
+
+        console.log(error);
+
+    }
+
+})
+
